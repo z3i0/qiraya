91044ae14a4080da882105bb4b3a81394b3faada
Ziad <zedohany@outlook.com>
2026-09-13 03:27:39 +0300

    chore: disable locale detection in i18n routing

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,6 +4,7 @@ import { createNavigation } from "next-intl/navigation";
 export const routing = defineRouting({
   locales: ["ar", "en"],
   defaultLocale: "ar",
+  localeDetection: false,
   localePrefix: "always",
 });
 
